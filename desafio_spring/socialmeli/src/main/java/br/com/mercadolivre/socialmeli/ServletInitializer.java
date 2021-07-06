5a9137d8318ea96b8b7de8964d7ed26b49d731a1
Pablo Rodrigo Lima dos Santos <pablo.santos@mercadolivre.com>
2021-07-06 15:37:59 -0300

    init: initializing repository inside folder 'desafio_spring'

---
+++ desafio_spring/socialmeli/src/main/java/br/com/mercadolivre/socialmeli/ServletInitializer.java
@@ -0,0 +1,13 @@
+package br.com.mercadolivre.socialmeli;
+
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+
+public class ServletInitializer extends SpringBootServletInitializer {
+
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+		return application.sources(SocialmeliApplication.class);
+	}
+
+}
---
+++ desafio_spring/socialmeli/src/test/java/br/com/mercadolivre/socialmeli/SocialmeliApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.mercadolivre.socialmeli;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SocialmeliApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
